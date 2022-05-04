2596d75e8f8a75ffb96efd0f068b928b9918dac5
alu0101333823 <alu0101333823@ull.edu.es>
2022-05-04 10:17:19 +0000

    Ejercicio hecho

--- src/ejercicio-clase/client.ts
+++ src/ejercicio-clase/client.ts
@@ -9,13 +9,18 @@ if (process.argv.length < 3) {
   const command = argv[2];
   const args = argv.splice(3);
   
+  client.write(JSON.stringify({
+    'command': command, 'arguments': args}), () => {
+  });
+  
+  
+  let output = '';
   client.on('data', (data) => {
-    console.log(`Conexión establecida: ${data}`);
-    client.write(JSON.stringify({
-      'command': command, 'arguments': args}), () => {
-      client.end();
-    });
+    output += data;
+    client.end();
+    
   }).on('end', () => {
+    console.log(output);
     console.log('Conexión finalizada sin errores');
   }).on('error', (err) => {
     if (err) {
--- src/ejercicio-clase/server.ts
+++ src/ejercicio-clase/server.ts
@@ -3,24 +3,27 @@ import * as net from 'net';
 
 const server = net.createServer((conection => {
   console.log('Conexión establecida!');
-  conection.write('Servidor en el puerto 60300');
 
   let msg: string = '';
   conection.on('data', (data) => {
     console.log(`El cliente me ha enviado: ${data}`);
     msg += data;
-  });
   
-  conection.on('close', () => {
     const msgJSON = JSON.parse(msg);
     const command = spawn(msgJSON.command, msgJSON.arguments);
-
-    console.log('Salida del comando solicitado: ');
 
     let commandOut = ''; 
     command.stdout.pipe(process.stdout);
     command.stdout.on('data', (piece) => commandOut += piece);
+
+    command.on('close', () => {
+      conection.write(`Salida del comando: \n${commandOut}`);
+    });
   });
+
+  conection.on('end', () => {
+    console.log('Cliente desconectado');
+  })
 
   conection.on('error', (err) => {
     if (err) {
